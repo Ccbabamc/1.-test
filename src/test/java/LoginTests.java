511fc614c146942a61a5c9f5e9e8759d59556571
Ccbabamc <159704809+Ccbabamc@users.noreply.github.com>
2024-11-07 20:28:08 +0300

    ..
    i

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,34 +13,47 @@ import java.time.Duration;
 
 public class LoginTests {
 
-    @Test@Step
+    @Test
+    @Description("Login test with invalid credentials")
     public void login() {
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 saniye bekleme süresi
 
         driver.get("https://www.trendyol.com/");
 
-        // Pop-up'ı kapatma
-        driver.findElement(By.className("modal-close")).click();
-
-        // Giriş butonuna tıklama
-        driver.findElement(By.className("link-text")).click();
-
-        // Giriş bilgilerini girme
-        driver.findElement(By.name("login email")).sendKeys("test@gmail.com");
-        driver.findElement(By.name("login-password")).sendKeys("deneme");
-
-        // Giriş butonuna tıklama
-        driver.findElement(By.xpath("//*[@id='login-register']/div[3]/div[1]/form/button")).click();
+        closePopup(driver);
+        clickLoginButton(driver);
+        enterCredentials(driver, "test@gmail.com", "deneme");
+        submitLogin(driver);
 
         // Hata mesajını bekleme ve doğrulama
         WebElement errorBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='error-box-wrapper']/span[2]")));
         String text = errorBox.getText();
         System.out.println(text);
 
-
         Assert.assertEquals("E-posta adresiniz ve/veya şifreniz hatalı.", text);
 
         driver.quit();
+    }
+
+    @Step("Close popup")
+    public void closePopup(WebDriver driver) {
+        driver.findElement(By.className("modal-close")).click();
+    }
+
+    @Step("Click login button")
+    public void clickLoginButton(WebDriver driver) {
+        driver.findElement(By.className("link-text")).click();
+    }
+
+    @Step("Enter credentials: {email}, {password}")
+    public void enterCredentials(WebDriver driver, String email, String password) {
+        driver.findElement(By.name("login email")).sendKeys(email);
+        driver.findElement(By.name("login-password")).sendKeys(password);
+    }
+
+    @Step("Submit login")
+    public void submitLogin(WebDriver driver) {
+        driver.findElement(By.xpath("//*[@id='login-register']/div[3]/div[1]/form/button")).click();
     }
 }
